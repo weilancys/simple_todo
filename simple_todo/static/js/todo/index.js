16c92b356c7f5b205dfc497eb413f71991185ebb
lightblue <weilancys@hotmail.com>
2021-03-03 11:34:36 +0800

    fixed a bug where todos with only black characters can be submitted from frontend.

--- simple_todo/static/js/todo/index.js
+++ simple_todo/static/js/todo/index.js
@@ -1,7 +1,9 @@
 window.addEventListener("DOMContentLoaded", function (e) {
     const todos = document.querySelectorAll(".todo-item");
     const btn_finish = document.querySelector(".btn-finish");
-    const todo_list_form = document.querySelector(".todo-list form");
+    const todo_list_form = document.querySelector(".todo-list-form");
+    const new_todo_form = document.querySelector(".new-todo-form");
+    const new_todo = document.querySelector("#new-todo");
 
     todo_list_form.addEventListener("submit", function (e) {
         let non_checked = true;
@@ -12,6 +14,13 @@ window.addEventListener("DOMContentLoaded", function (e) {
         }
         if (non_checked) {
             e.preventDefault();
+        }
+    });
+
+    new_todo_form.addEventListener("submit", function (e) {
+        if (new_todo.value.trim() == "") {
+            e.preventDefault();
+            return false;
         }
     });
     
